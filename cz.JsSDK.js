c9fa1b5317b8038609b1ebf4d071eb015e90662e
KTBZay <97396492+KTBZay@users.noreply.github.com>
2022-08-11 14:02:17 -0400

    Add files via upload

--- cz.JsSDK.js
+++ cz.JsSDK.js
@@ -15,12 +15,25 @@ const cz_CreateProjectPath = (path) => {
         fs.mkdirSync(path, { recursive: true });
     }
 }
-const cz_WriteTofile = (path) => {
-    fs.writeFileSync(`${path}/discord_bot.js`, `const {Client} = require('discord.js') \n const config = { token: '${configuration.Discord_Bot_Token}', prefix: '${configuration.Discord_Bot_Prefix}'}\n const client = new Client({ Intents: ["GUILD_MESSAGING", "GUILDS"] } \n client.on('ready', () => { console.log('Logged in') } ; \n client.login(config.token)`);
+const cz_WriteTofile = (path,token,prefix) => {
+    fs.writeFileSync(`${path}/discord_bot.js`, `const {Client} = require('discord.js') \n const config = { token: '${token}', prefix: '${prefix}'}\n const client = new Client({ Intents: ["GUILD_MESSAGING", "GUILDS"] } \n client.on('ready', () => { console.log('Logged in') } ; \n client.login(config.token)`);
+}
+
+const czRunTimeService = (app,path, { name = '', type = '', author = '' }) => {
+    setTimeout(() => {
+        const MainDir = fs.mkdir(`${path}/data/runtime/`, { recursive: true }, () => { });
+        fs.writeFileSync(`${path}/data/runtime/${app}.config.js`,
+            `
+			const RunBroker = {serviceApp: '${app}', serviceName: '${name}', serviceType: '${type}', serviceAuthor: '${author}'}; \n
+			 console.log('ServiceWorker: ${app} has now started!')
+			module.exports = { RunBroker }`)
+
+    }, 3000)
 }
 module.exports = {
     FileCreator,
     FolderCreator,
     cz_CreateProjectPath,
-    cz_WriteTofile
+    cz_WriteTofile,
+    czRunTimeService
 }
